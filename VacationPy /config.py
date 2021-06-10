37f93cee50d133ff345df4c395c1350c6a9bb461
Rodney DAVERMANN <davermannrodney@gmail.com>
2021-06-10 02:18:27 -0400

    Python API Challenge

--- VacationPy /config.py
+++ VacationPy /config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "6d4b83e04d0e11584042a0a2b7d295d4"
+weather_api_key = "fa70aa2e3d626a254b8c3ddacc6aec5e"
 
 # Google API Key
 google_api_key = "AIzaSyDTDGbftWDjg8aJMCtMLWbOmq8ftRzPdj4"
---
+++ WeatherPy/config.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "fa70aa2e3d626a254b8c3ddacc6aec5e"
+
+# Google API Key
+google_api_key = "AIzaSyDTDGbftWDjg8aJMCtMLWbOmq8ftRzPdj4"
